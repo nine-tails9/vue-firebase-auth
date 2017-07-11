4a2921c0f3f6b61ab24de621abeae62e44a6e274
Deepanshu Gandhi <Deepanshu Gandhi>
2017-07-12 00:10:32 +0530

    Enabled google and email login

---
+++ src/helpers/firebaseConfig.js
@@ -0,0 +1,8 @@
+export const config = {
+  apiKey: "AIzaSyCV34CTth61sfHB2r90mIM0YaU8GfkpPrc",
+  authDomain: "fb-auth-2f6f6.firebaseapp.com",
+  databaseURL: "https://fb-auth-2f6f6.firebaseio.com",
+    projectId: "fb-auth-2f6f6",
+    storageBucket: "fb-auth-2f6f6.appspot.com",
+    messagingSenderId: "452046800447"
+};
--- src/main.js
+++ src/main.js
@@ -1,7 +1,23 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import VueRouter from 'vue-router'
+import firebase from 'firebase'
+import firebaseui from 'firebaseui';
+import router from './router'
+import {config} from './helpers/firebaseConfig'
+Vue.use(VueRouter);
 new Vue({
+  router,
+  created() {
+    firebase.initializeApp(config);
+    firebase.auth().onAuthStateChanged((user) => {
+      if(user) {
+        this.$router.push('/success')
+      } else {
+        this.$router.push('/auth')
+      }
+     });
+    },
   el: '#app',
   render: h => h(App)
 })
